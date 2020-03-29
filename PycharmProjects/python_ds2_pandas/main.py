a52e7369932b9d8e8822ba1fc6cfca5d0d9545fb
kgawelek <konradgawelek@gmail.com>
2020-03-29 18:58:23 +0200

    import danych w pandas

--- PycharmProjects/python_ds2_pandas/main.py
+++ PycharmProjects/python_ds2_pandas/main.py
@@ -63,3 +63,57 @@ print(incidents.filter(items=['18 - 24', '25 - 39']))
 incidents.where(incidents <= 1000, inplace=True)
 incidents.dropna(inplace=True)
 print(incidents)
+
+names_list =['Albania', 'Austria', 'Belarus', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France',
+             'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Italy',
+             'Latvia', 'Lithuania', 'Luxembourg', 'Macedonia', 'Malta', 'Montenegro', 'Netherlands',
+             'Norway', 'Poland', 'Portugal', 'Romania', 'Russia', 'Serbia', 'Slovenia', 'Spain',
+             'Sweden', 'Switzerland', 'United Kingdom', 'Turkey', 'Ukraine']
+
+energy_list2010 = [1947,8347,3564,8369,4560,3814,4623,6348,6328,6506,16483,7736,7264,5318,3876,
+51440,5911,5494,3230,3471,16830,3521,4171,5420,7010,24891,3797,4959,2551,
+6410,4359,6521,5707,14934,8175,2498,3550,5701]
+
+energy_list2012 = [2118,8507,3698,7987,4762,3819,4057,6305,6039,6689,15687,7344,7270,5511,3919,
+53203,5665,5398,3588,3608,14696,3626,4761,5416,6871,23658,3899,4736,2604,
+6617,4387,6778,5573,14290,7886,2794,3641,5452]
+
+names_series = pd.Series(names_list)
+energy_2010_series = pd.Series(energy_list2010)
+energy_2012_series = pd.Series(energy_list2012)
+
+mean2010 = energy_2010_series.mean()
+mean2012 = energy_2012_series.mean()
+
+print(mean2010, ' ', mean2012)
+
+filter_above_mean2010 = energy_2010_series > mean2010
+filter_above_mean2012 = energy_2012_series > mean2012
+
+print(names_series.where(filter_above_mean2010 & filter_above_mean2012).dropna())
+
+education = pd.read_csv("survey_results_public.csv", usecols=['FormalEducation'])
+
+education = pd.read_csv("survey_results_public.csv", usecols=['FormalEducation'], squeeze=True)
+
+
+all_info = pd.read_csv("survey_results_public.csv")
+country = all_info['Country']
+#print(type(country))
+filter_USA = country == 'United States'
+#print(filter_USA.head())
+
+print(education.where(filter_USA).dropna().head())
+
+
+salary = pd.read_csv("survey_results_public.csv", usecols=["Salary"], squeeze=True)
+salary.dropna(inplace=True)
+print(salary.head())
+print(len(salary))
+print(min(salary), '  ', max(salary))
+print(list(salary))
+print(dict(salary))
+list_salary_sorted = sorted(list(salary))
+print(list_salary_sorted[:5])
+salary.name = "Salary of a person"
+print(salary.name)
